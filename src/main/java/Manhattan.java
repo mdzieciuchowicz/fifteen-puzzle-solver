4105aacd7020e6ed0cab46d0a282cd1e3649e82d
jc <kczarnecok@wp.pl>
2021-11-11 14:33:45 +0100

    Fix Manhattan metric

--- src/main/java/Manhattan.java
+++ src/main/java/Manhattan.java
@@ -5,15 +5,15 @@ public class Manhattan implements Metric{
         int x, y, goalX, goalY;
 
         for (int i = 0; i < node.getCurrentState().getTable().getTable().length; i++) {
-            if (node.getCurrentState().getTable().getTable()[i] != i + 1 && node.getCurrentState().getTable().getTable()[i] != 0) {}
+            if (node.getCurrentState().getTable().getTable()[i] != i + 1 && node.getCurrentState().getTable().getTable()[i] != 0) {
+                //przeliczanie na współrzędne w 2 wymiarach
+                goalX = (node.getCurrentState().getTable().getTable()[i] - 1) % node.getCurrentState().getTable().getCols();
+                goalY = (node.getCurrentState().getTable().getTable()[i] - 1) / node.getCurrentState().getTable().getRows();
+                x = i % node.getCurrentState().getTable().getCols();
+                y = i / node.getCurrentState().getTable().getRows();
 
-            //przeliczanie na współrzędne w 2 wymiarach
-            goalX = (node.getCurrentState().getTable().getTable()[i] - 1) % node.getCurrentState().getTable().getCols();
-            goalY = (node.getCurrentState().getTable().getTable()[i] - 1) / node.getCurrentState().getTable().getRows();
-            x = i % node.getCurrentState().getTable().getCols();
-            y = i / node.getCurrentState().getTable().getRows();
-
-            distance += (Math.abs(goalX - x) + Math.abs(goalY - y));
+                distance += (Math.abs(goalX - x) + Math.abs(goalY - y));
+            }
         }
 
         return distance;
